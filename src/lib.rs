b6334c596cca90e3f682b7389e5039de916af514
SymmetricChaos <ajfraebel@gmail.com>
2022-09-05 07:58:02 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::VecDeque;
+use std::{collections::VecDeque, ops::Index};
 
 use rand::Rng;
 use rand::seq::SliceRandom;
@@ -264,6 +264,20 @@ impl<T> Deck<T> {
         rand::thread_rng().gen_range(0..self.cards.len())
     }
 
+
+
+    /// Get a reference to the nth card.
+    pub fn get(&self, n: usize) -> Option<&T> {
+        self.cards.get(n)
+    }
+
+    /// Get a mutable reference to the nth card.
+    pub fn get_mut(&mut self, n: usize) -> Option<&mut T> {
+        self.cards.get_mut(n)
+    }
+
+
+
     /// Draw the top card of the deck.
     pub fn draw_top(&mut self) -> Option<T> {
         self.cards.pop_front()
@@ -318,21 +332,37 @@ impl<T> Deck<T> {
 
 
 
-    /// Cut the deck at nth position and stack it again.
+    /// Cut the deck at nth position.
     pub fn cut_nth(&mut self, n: usize) {
         self.cards.rotate_left(n)
     }
 
-    /// Cut the deck at a random position and stack it again.
+    /// Cut the deck at a random position.
     pub fn cut_random(&mut self) {
-        let mut rng = rand::thread_rng();
-        let n = rng.gen_range(0..self.cards.len());
-        self.cards.rotate_left(n)
+        self.cards.rotate_left(self.uniform())
     }
 
     /// Cut the deck following a binomial distribution, simulating human selection.
     pub fn cut_binom(&mut self) {
         self.cards.rotate_left(self.binom())
+    }
+
+
+
+
+    /// Split the deck at the nth position. Retains the top of the split and returns the bottom.
+    pub fn split_nth(&mut self, n: usize) -> Deck<T> {
+        Deck{ cards: self.cards.split_off(n) }
+    }
+
+    /// Split the deck at a random position. Retains the top of the split and returns the bottom.
+    pub fn split_random(&mut self) -> Deck<T> {
+        self.split_nth(self.uniform())
+    }
+
+    /// Split the deck following a binomial distribution, simulating human selection. Retains the top of the split and returns the bottom.
+    pub fn split_binom(&mut self) -> Deck<T> {
+        self.split_nth(self.binom())
     }
 
 
@@ -345,19 +375,82 @@ impl<T> Deck<T> {
 
     /// Perform a single Bayer-Diaconis riffle of the deck. This is a mathematically poor shuffle that simulates a single riffle shuffle. For good mixing several riffles are needed.
     pub fn riffle(&mut self) {
-        let bin = Binomial::new(self.cards.len().try_into().unwrap(), 0.5).unwrap();
-        let n = bin.sample(&mut rand::thread_rng());
+        let n = self.split_binom();
+        todo!()
 
+    }
+}
+
+impl<T, const N: usize> From<[T;N]> for Deck<T> {
+    fn from(arr: [T;N]) -> Self {
+        Deck { cards: VecDeque::from(arr) }
+    }
+}
+
+impl<T> From<Vec<T>> for Deck<T> {
+    fn from(vec: Vec<T>) -> Self {
+        Deck { cards: VecDeque::from(vec) }
+    }
+}
+
+impl<T> From<VecDeque<T>> for Deck<T> {
+    fn from(vec: VecDeque<T>) -> Self {
+        Deck { cards: VecDeque::from(vec) }
+    }
+}
+
+impl<T> FromIterator<T> for Deck<T> {
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+        let iterator = iter.into_iter();
+        let (lower, _) = iterator.size_hint();
+        let mut cards = VecDeque::with_capacity(lower);
+        cards.extend(iterator);
+        Deck { cards }
     }
 }
 
 
 
+
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn draw_top() {
+        let mut deck = Deck::from([1,2,3,4,5,6]);
+        assert_eq!(deck.draw_top().unwrap(),1)
+    }
+
+    #[test]
+    fn draw_bottom() {
+        let mut deck = Deck::from([1,2,3,4,5,6]);
+        assert_eq!(deck.draw_bottom().unwrap(),6)
+    }
+
+    #[test]
+    fn draw_nth() {
+        let mut deck = Deck::from([1,2,3,4,5,6]);
+        assert_eq!(deck.draw_nth(3).unwrap(),4)
+    }
+
+
+    #[test]
+    fn place_top() {
+        let mut deck = Deck::from([1,2,3,4,5,6]);
+        deck.place_top(100)
+    }
+
+    #[test]
+    fn place_bottom() {
+        let mut deck = Deck::from([1,2,3,4,5,6]);
+        deck.place_bottom(100)
+    }
+
+    #[test]
+    fn place_nth() {
+        let mut deck = Deck::from([1,2,3,4,5,6]);
+        deck.place_nth(3,100)
     }
 }
