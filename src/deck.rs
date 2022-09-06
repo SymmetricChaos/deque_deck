70e3ad3213300385c3cd0523f4d7132f84cf9279
SymmetricChaos <ajfraebel@gmail.com>
2022-09-06 11:05:30 -0400

    Update deck.rs

--- src/deck.rs
+++ src/deck.rs
@@ -1,9 +1,10 @@
-use std::collections::VecDeque;
+use std::{collections::VecDeque, fmt::Debug};
 
 use rand::seq::SliceRandom;
 use rand::Rng;
 use rand_distr::{Bernoulli, Binomial, Distribution};
 
+#[derive(Clone, Debug)]
 pub struct Deck<T> {
     cards: VecDeque<T>,
 }
@@ -15,7 +16,7 @@ impl<PlayingCard> Deck<PlayingCard> {
     }
 }
 
-impl<T> Deck<T> {
+impl<T: Debug> Deck<T> {
     fn bern(p: f64) -> bool {
         let bern = Bernoulli::new(p).unwrap();
         bern.sample(&mut rand::thread_rng())
@@ -36,6 +37,74 @@ impl<T> Deck<T> {
 
     pub fn with_capacity(n: usize) -> Deck<T> {
         Deck::from(VecDeque::with_capacity(n))
+    }
+
+    /// Consumes two Decks and returns the result of riffling them together using the Gilbert-Shannon-Reeds algorithm. This is a slow and statistically poor quality shuffle that simulates a single riffle shuffle. For good mixing several riffles are needed.
+    pub fn from_riffle(mut left: Deck<T>, mut right: Deck<T>) -> Deck<T> {
+        let n = left.len() + right.len();
+        let mut new: Deck<T> = Deck::with_capacity(n);
+        for _ in 0..n {
+            // We're going to hope decks are restricted to billions of cards here
+            let l = left.len() as f64;
+            let r = right.len() as f64;
+            if Self::bern(r / (l + r)) {
+                match left.draw_top() {
+                    Some(card) => new.place_bottom(card),
+                    None => {
+                        new.extend(right);
+                        break;
+                    }
+                }
+            } else {
+                match right.draw_top() {
+                    Some(card) => new.place_bottom(card),
+                    None => {
+                        new.extend(left);
+                        break;
+                    }
+                }
+            }
+        }
+        new
+    }
+
+    /// Combine the Deck with another deck, in place, via a riffle shuffle.
+    pub fn riffle_with(&mut self, mut right: Deck<T>) {
+        if self.len() == 0 {
+            self.extend(right);
+            return;
+        }
+
+        let mut l_len = self.len();
+        let mut r_len = right.len();
+        let mut cursor = 0;
+
+        loop {
+            if r_len == 0 {
+                break;
+            }
+            let l = l_len as f64;
+            let r = r_len as f64;
+
+            // If the right branch is chosen, place the card at the cursor position
+            // and reduce the length of the right side
+            if Self::bern(r / (l + r)) {
+                match right.draw_top() {
+                    Some(card) => {
+                        self.place_nth(cursor, card);
+                        r_len -= 1;
+                    }
+                    None => {
+                        self.extend(right);
+                        break;
+                    }
+                }
+            // If the left branch is chosen just reduce the length of the left size.
+            } else {
+                l_len -= 1;
+            }
+            cursor += 1;
+        }
     }
 
     pub fn len(&self) -> usize {
@@ -154,47 +223,22 @@ impl<T> Deck<T> {
         self.split_nth(n)
     }
 
-    /// Perform a Fisher-Yates shuffle on the deck. This is a mathematically correct shuffle that gives every card an equal chance of ending up at any postion. For a simulated human shuffle see riffle.
+    /// Perform a Fisher-Yates shuffle on the deck. This is a mathematically correct shuffle that gives every card an equal chance of ending up at any postion.
     pub fn shuffle(&mut self) {
         let mut rng = rand::thread_rng();
         self.cards.make_contiguous().shuffle(&mut rng);
     }
 }
 
-impl<T: Clone> Deck<T> {
-    /// Perform a single Gilbert-Shannon-Reeds riffle of the deck. This is a slow and statistically poor quality shuffle that simulates a single riffle shuffle. For good mixing several riffles are needed.
-    pub fn riffle(&mut self) {
-        let n = self.cards.len();
-        let mut new: Deck<T> = Deck::with_capacity(n);
-        let cards = Deck::from(self.cards.clone());
-        let (mut left, mut right) = cards.split_binom();
-        for _ in 0..n {
-            // We're going to hope decks are restricted to billions of cards here
-            let l = left.len() as f64;
-            let r = right.len() as f64;
-            if Self::bern(l / (r + l)) {
-                match left.draw_top() {
-                    Some(card) => new.place_top(card),
-                    None => {
-                        new.extend(right);
-                        break;
-                    }
-                }
-            } else {
-                match right.draw_top() {
-                    Some(card) => new.place_top(card),
-                    None => {
-                        new.extend(left);
-                        break;
-                    }
-                }
-            }
-        }
-        *self = new;
+impl<T: Clone + Debug> Deck<T> {
+    /// Perform a single riffle shuffle of the deck. See .riffle() for details.
+    pub fn shuffle_riffle(&mut self) {
+        let right = self.split_off_binom();
+        self.riffle_with(right);
     }
 
-    /// Perform a perfect faro shuffle if the deck contains an even number of cards.
-    pub fn faro(&mut self, out: bool) -> Result<(), &'static str> {
+    /// Perform a perfect riffle shuffle (aka a faro shuffle). The deck must contain an even number of cards. This is not a fair shuffle.
+    pub fn shuffle_faro(&mut self, out: bool) -> Result<(), &'static str> {
         let n = self.cards.len();
         if n % 2 == 0 {
             return Err("a faro shuffle requires an even number of cards");
@@ -214,6 +258,20 @@ impl<T: Clone> Deck<T> {
                 }
             }
         }
+        Ok(())
+    }
+
+    /// Perform a Gilbreath shuffle on the deck that uses n cards. This is not a fair shuffle.
+    pub fn shuffle_gilbreath(&mut self, n: usize) -> Result<(), &'static str> {
+        if n > self.len() {
+            return Err("n must be less than the number of cards in the deck");
+        }
+        let mut new: Deck<T> = Deck::with_capacity(n);
+        for _ in 0..n {
+            new.place_top(self.draw_top().unwrap())
+        }
+        self.riffle_with(new);
+
         Ok(())
     }
 }
@@ -309,5 +367,24 @@ mod test_deck {
         let mut deck = Deck::from([1, 2, 3, 4, 5, 6]);
         deck.place_nth(3, 100);
         assert_eq!(deck.get(3).unwrap(), &100);
+    }
+
+    // #[test]
+    // fn riffle() {
+    //     for _ in 0..10 {
+    //         let left = Deck::from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    //         let right = Deck::from([10, 11, 12, 13, 14, 15, 16, 17, 18, 19]);
+    //         println!("{:?}", Deck::riffle(left, right))
+    //     }
+    // }
+
+    #[test]
+    fn riffle_in_place() {
+        for _ in 0..10 {
+            let mut left = Deck::from([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+            let right = Deck::from([10, 11, 12, 13, 14, 15, 16, 17, 18, 19]);
+            left.riffle_with(right);
+            println!("{:?}", left)
+        }
     }
 }
